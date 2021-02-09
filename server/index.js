9763ac9ffa621a1d072e4b8cafe85ba7aee383b2
Victor Ung <vung@ucsd.edu>
2021-02-08 23:00:45 -0800

    connected mongodb

--- server/index.js
+++ server/index.js
@@ -1,2 +1,22 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+
+const dbUrl = process.env.DB_URL;
+
+const mongoose = require("mongoose");
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.on("error", console.error.bind(console, "connection error:"));
+connection.once("open", () => {
+    console.log("Database connected");
+});
+
+app.listen(3000, () => {
+    console.log("APP IS LISTENING ON PORT 3000!");
+});
