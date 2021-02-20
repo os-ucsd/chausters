5e8533588599b591ef529fcb7049a4d96f441cdc
Varun Singh <dualsaturn@gmail.com>
2021-02-20 11:46:40 -0800

    setup react router, added login and register route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Register from './components/Register';
+import Login from './components/Login';
+import Home from './components/Home';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/login' component={Login}/>
+        <Route path='/register' component={Register}/>
+      </Switch>
     </div>
   );
 }
